1104ed6082b15bbde99583f02fc6a1c363b9358a
sharganow <sharganow@gmail.com>
2023-02-21 02:14:32 +0500

    I am tired

--- JsonSearch.java
+++ JsonSearch.java
@@ -2,12 +2,66 @@
         Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
         Если значение null, то параметр не должен попадать в запрос.
         Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}  //*/
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 public class JsonSearch {
     static String[] keys = new String[] {"name", "country", "city", "age"};
-    static String[] select_keys = new String[keys.length];
-    static String[] files_names;
+    static String[] selectKeys = new String[keys.length];
+    static String[] filesNames = new String[keys.length];
+    static boolean[] filesChanged = new boolean[keys.length];
+    static StringBuilder[][] findIndexes = new StringBuilder[keys.length][];
+
+
+    static void startInitialFiles(){
+        String pathProject = System.getProperty ("user.dir" );
+        String pathDir = pathProject .concat("/JnSh");
+        File dir = new File(pathDir);
+        System.out.println(dir.getAbsolutePath ());
+        if (dir.mkdir()) {
+            try {
+                if (dir.list().length == 0) {
+                    String pathFile = pathDir.concat("/students.base");
+                    File file = new File(pathFile);
+                    if(file.createNewFile()){
+                        FileWriter fileWriter = new FileWriter(file, false);
+                        fileWriter.write("length:0");
+                        fileWriter.append(System.lineSeparator());
+                        fileWriter.flush();
+                        fileWriter.close();
+                        System.out.println("Файл базы данных студентов создан, он пуст, можно это заложить в файл");
+                        File[] files = new File[keys.length];
+                        byte i = 0;
+                        for (String key : keys){
+                            pathFile = pathDir.concat("/" + key + ".keys");
+                            files[i] = new File(pathFile);
+                            if(files[i].createNewFile()){
+                                FileWriter filesWriter = new FileWriter(files[i], false);
+                                filesWriter.write("length:0");
+                                filesWriter.append(System.lineSeparator());
+                                filesWriter.flush();
+                                filesWriter.close();
+                                System.out.printf("Поисковый Файл %s.keys  базы данных студенитов создан, он пуст, можно это заложить в файл\n", key);
+                            }
+                        }
+                    }
+                    else{
+                        System.out.println( "Этого не может быть, " +
+                                "файла не может существовать, " +
+                                "мы дирректорию-то только что создали," +
+                                "это тоже можно заложить в файл"            );
+                    }
+                }
+            }
+            catch(Exception e){
+                System.out.printf("Что-то не пошло: %s, это можно залогжить в файл", e.getMessage());
+            }
+        } else {
+            System.out.println("-");
+        }
+        for (String fname : dir.list()) {
+            System.out.printf("%s \t%d\n",fname, fname.length());
+        }
+    }
 
     static int getIndex(String[] fields, String value){
         int index = -1;
@@ -25,9 +79,9 @@ public class JsonSearch {
             Scanner iScanner = new Scanner(System.in);
             for (String key : keys) {
                 System.out.printf("Введите %s: ", key);
-                select_keys[getIndex(keys, key)] = iScanner.nextLine();
-                if (select_keys[getIndex(keys, key)].length() == 0) {
-                    select_keys[getIndex(keys, key)] = "null";
+                selectKeys[getIndex(keys, key)] = iScanner.nextLine();
+                if (selectKeys[getIndex(keys, key)].length() == 0) {
+                    selectKeys[getIndex(keys, key)] = "null";
                 }
                 else{
                     seek = true;
@@ -57,47 +111,9 @@ public class JsonSearch {
     }
 
     public static void main(String[] args){
-        String pathProject = System.getProperty ("user.dir" );
-        String pathDir = pathProject .concat("/JnSh");
-        File dir = new File(pathDir);
-        System.out.println(dir.getAbsolutePath ());
-        if (dir.mkdir()) {
-            try {
-                if (dir.list().length == 0) {
-                    String pathFile = pathDir.concat("/students.base");
-                    File file = new File(pathFile);
-                    if(file.createNewFile()){
-                        System.out.println("Файл базы данных студентов создан, он пуст, можно это заложить в файл");
-                        File[] files = new File[keys.length];
-                        byte i = 0;
-                        for (String key : keys){
-                            pathFile = pathDir.concat("/" + key + ".keys");
-                            files[i] = new File(pathFile);
-                            if(files[i].createNewFile()){
-                                System.out.printf("Поисковый Файл %s.keys  базы данных студенитов создан, он пуст, можно это заложить в файл\n", key);
-                            }
-                        }
-                    }
-                    else{
-                        System.out.println( "Этого не может быть, " +
-                                            "файла не может существовать, " +
-                                            "мы дирректорию-то только что создали," +
-                                            "это тоже можно заложить в файл"            );
-                    }
-                }
-            }
-            catch(Exception e){
-                System.out.printf("Что-то не пошло: %s, это можно залогжить в файл", e.getMessage());
-            }
-        } else {
-            System.out.println("-");
-        }
-        for (String fname : dir.list()) {
-            System.out.printf("%s \t%d\n",fname, fname.length());
-        }
-
-        while(fillSelect(select_keys)){
-            System.out.println(seekStudent(select_keys));
+        startInitialFiles();
+        while(fillSelect(selectKeys)){
+            System.out.println(seekStudent(selectKeys));
         }
 
     }
